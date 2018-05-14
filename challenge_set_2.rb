0a79b7ed1aed2f6cab5d33dd60adab56fe6e03d5
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-14 17:09:41 +0100

    Challenge set 2: Second Solution

--- challenge_set_2.rb
+++ challenge_set_2.rb
@@ -1,3 +1,11 @@
 def value_plus_one(hash)
   hash.each { |key, val| hash[key] = val + 1 }
 end
+
+def sort_only_values(hash)
+  keys = hash.keys
+  values = hash.values.sort
+  new_hash = {}
+  keys.each_with_index { |key, index| new_hash[key] = values[index]}
+  new_hash
+end
